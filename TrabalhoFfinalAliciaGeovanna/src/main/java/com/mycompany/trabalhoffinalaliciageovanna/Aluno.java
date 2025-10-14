6d9a27a16759ca2b287dddb8e82af4e372de85eb
aliciassantos <aliciasantossfs@gmail.com>
2025-10-14 09:58:24 -0300

    Laboratório 14/10

---
+++ TrabalhoFfinalAliciaGeovanna/src/main/java/com/mycompany/trabalhoffinalaliciageovanna/Aluno.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhoffinalaliciageovanna;
+
+/**
+ *
+ * @author DTIC
+ */
+public class Aluno {
+     //Declaração das variáveis dos alunos
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    private String nome;
+    private String CPF;
+    private String telefone;
+    private int idade;
+}
